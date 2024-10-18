eadc11d467ed6e4f25365990f25de55a734645d4
caun94 <cristobalugarte6@gmail.com>
2024-10-18 10:50:36 -0300

    updates

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -7,7 +7,7 @@
 
     
     <div class="py-12">
-        <div class="max-w-2xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 @if(session('status'))
                     <div>
